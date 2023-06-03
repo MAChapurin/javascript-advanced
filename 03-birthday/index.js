f12fbb6a3ce1910e902a7ca200bbfa366d548ae6
Michael <machapurin@icloud.com>
2023-06-04 00:25:25 +0300

    changed check age

--- 03-birthday/index.js
+++ 03-birthday/index.js
@@ -1,19 +1,17 @@
 function checkAge(date) {
   const now = new Date();
-  const year = 3.154e10;
   const birthday = new Date(date);
   if (now.getFullYear() - birthday.getFullYear() > 14) return true;
-  let birthdayYear = now.getFullYear() - birthday.getFullYear();
-  if (birthdayYear === 14) {
-    birthdayYear += 1;
-  }
+  if (now.getFullYear() - birthday.getFullYear() < 14) return false;
   return (
-    birthday.getMonth() >= now.getMonth() && birthday.getDate() >= now.getDate()
+    birthday.getMonth() < now.getMonth() ||
+    (birthday.getMonth() === now.getMonth() &&
+      birthday.getDate() <= now.getDate())
   );
 }
 
 console.log(checkAge('2009-06-01'));
-console.log(checkAge('2009-06-02'));
-console.log(checkAge('2009-06-03'));
 console.log(checkAge('2009-06-04'));
-console.log(checkAge('2010-06-01'));
+console.log(checkAge('2009-07-03'));
+console.log(checkAge('2009-06-06'));
+console.log(checkAge('2019-07-07'));
